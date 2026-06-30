740fdf2eb81c84bc1a7cf61935e737c1428eb2f3
KingBuchi10 <nezianya9@gmail.com>
2026-06-30 22:00:04 +0100

    Comm

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
+// const BannerData = {
+//     discount: "50% OFF",
+//     title: "Summer Sale",
+//     description: "Get ready for the hottest deals of the season! Our summer sale is here, offering you incredible discounts on a wide range of products. Don't miss out on this opportunity to save big and upgrade your summer essentials.",
+//     image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+//     title1:"Air Solo bass",
+//     title2:"Wireless Earbuds",
+//     title3:"$99.99",
+    
 
+// }
 
 const Banner = () => {
   return (
     <div>
       <div>
-        
+
       </div>
     </div>
   )
